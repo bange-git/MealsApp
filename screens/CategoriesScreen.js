8ac9a891d33f524f0a9b724a7f16fbd26b98ee37
bange-git <sebastienbange@gmail.com>
2022-12-21 14:44:41 +0100

    displaying meals details

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -2,13 +2,20 @@ import { View, FlatList } from "react-native";
 import CategoriesGridTitle from "../components/CategoriesGridTitle";
 import { CATEGORIES } from "../data/dummy-data";
 
-const CategoriesScreen = ({navigation}) => {
-    
+const CategoriesScreen = ({ navigation }) => {
   const renderCategoryItem = ({ item }) => {
     const pressHandler = () => {
-        navigation.navigate('MealsOverview')
-    }
-    return <CategoriesGridTitle title={item.title} color={item.color} onPress={pressHandler} />;
+      navigation.navigate("MealsOverview", {
+        categoryId: item.id,
+      });
+    };
+    return (
+      <CategoriesGridTitle
+        title={item.title}
+        color={item.color}
+        onPress={pressHandler}
+      />
+    );
   };
   return (
     <View>
--- screens/MealsOverViewScreen.js
+++ screens/MealsOverViewScreen.js
@@ -1,9 +1,28 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
+import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
-const MealsOverViewScreen = () => {
+const MealsOverViewScreen = ({ route }) => {
+  const catId = route.params.categoryId;
+
+  const displayMeals = MEALS.filter((meal) => {
+    return meal.categoryIds.indexOf(catId) >= 0;
+  });
   return (
     <View>
-      <Text>Meals Detals</Text>
+      <Text>Meals Detals - {catId}</Text>
+      <FlatList
+        data={displayMeals}
+        renderItem={({ item }) => (
+          <MealItem
+            title={item.title}
+            imageUrl={item.imageUrl}
+            affordability={item.affordability}
+            complexity={item.complexity}
+            duration={item.duration}
+          />
+        )}
+      />
     </View>
   );
 };
