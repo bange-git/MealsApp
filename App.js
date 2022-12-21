268d6a591eba7242acf9d5181ebbd3809eebde36
bange-git <sebastienbange@gmail.com>
2022-12-21 14:55:14 +0100

    styling screen headers and backgrounds

--- App.js
+++ App.js
@@ -5,15 +5,30 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MealsOverViewScreen from "./screens/MealsOverViewScreen";
 
 export default function App() {
-
   const Stack = createNativeStackNavigator();
   return (
     <>
-      <StatusBar style="dark" />
+      <StatusBar style="light" />
       <NavigationContainer>
-        <Stack.Navigator>
-           <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
-           <Stack.Screen name="MealsOverview" component={MealsOverViewScreen} />
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: "#351401" },
+            headerTintColor: "white",
+            contentStyle: { backgroundColor: "#3f2f25" },
+          }}
+        >
+          <Stack.Screen
+            name="MealsCategories"
+            component={CategoriesScreen}
+            options={{
+              title: "All Categories",
+            }}
+          />
+          <Stack.Screen name="MealsOverview" component={MealsOverViewScreen}
+          options={{
+            title: "Meals Overview",
+          }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
